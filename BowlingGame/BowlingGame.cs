0276d86091c770068ae6db06f20c2f4ab8c914f7
vrek <vrek@users.noreply.github.com>
2025-09-11 21:58:32 -0500

    Add project files.

---
+++ BowlingGame/BowlingGame.cs
@@ -0,0 +1,21 @@
+﻿
+namespace BowlingGameKata;
+
+public class BowlingGame
+{
+    private int _score = 0;
+    public static void Main(string[] args)
+    {
+        // Entry point logic here
+    }
+
+    public void Roll(int pins)
+    {
+        _score += pins;
+    }
+
+    public int Score()
+    {
+        return _score;
+    }
+}
---
+++ TestBowlingGame/PassingTest.cs
@@ -0,0 +1,41 @@
+using BowlingGameKata;
+
+namespace TestBowlingGame;
+
+public class PassingTest
+{
+    private BowlingGame _game;
+
+    public PassingTest()
+    {
+        _game = new BowlingGame();
+    }
+
+    private void RollMany(int pins, int rolls)
+    {
+        for (var i = 0; i < 20; i++)
+        {
+            _game.Roll(pins);
+        }
+    }
+
+    [Fact]
+    public void GivenAGameOfAllZeroShouldReturnAZeroScore()
+    {
+
+
+        var pins = 0;
+        var rolls = 20;
+        RollMany(pins, rolls);
+        Assert.Equal(0, _game.Score());
+    }
+
+    [Fact]
+    public void GivenAGameOfAllOneShouldReturnATwentyScore()
+    {
+        var pins = 1;
+        var rolls = 20;
+        RollMany(pins, rolls);
+        Assert.Equal(20, _game.Score());
+    }
+}
